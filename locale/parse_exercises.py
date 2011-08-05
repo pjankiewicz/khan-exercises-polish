a34048f4e0380ef0018e214dcd663ea008241cf7
Pawel <p.jankiewicz@gmail.com>
2011-08-05 17:02:03 +0200

    Refactoring

--- locale/parse_exercises.py
+++ locale/parse_exercises.py
@@ -1,29 +1,20 @@
+#encoding: utf-8
 import re
 import os
 import pprint
 import string
+import sqlite3
+from remass.remass import RegexMass
 
 recomp = re.compile
 
-"""
-Parses exercise files
-"""
-
+EXERCISES_PATH = os.path.join(os.getcwd(),"..","exercises","*.html")
+TRANSLATION_FILE = "en.js"
 RE_OPTIONS = re.M|re.I|re.DOTALL
-RE_INCLUDE = [
-    recomp("<p([^<>]*)>(.*?)<\/p>",RE_OPTIONS),
+TRANSLATION_TAGS_RE = [
+    recomp("<p[^<>]*>(.*?)<\/p>",RE_OPTIONS),
     recomp("<title>(.*?)<\/title>",RE_OPTIONS),
 ]
-
-# ignore text that includes only <code>, <var>
-RE_IGNORE = [
-    recomp("^<code>[^<>]+<\/code>$",RE_OPTIONS),
-    recomp("^<var>[^<>]+<\/var>$",RE_OPTIONS),
-    recomp("^<code><var>[^<>]+<\/var><\/code>$",RE_OPTIONS),
-    # ignore the blocks with defined t() [translation function]
-    recomp("_\(.*?\)",RE_OPTIONS)
-]
-
 CLEAN_RULES = [
     # html tags &middot;
     recomp("&[a-z]+;",RE_OPTIONS),
@@ -54,55 +45,94 @@ CLEAN_RULES = [
 ]
 
 
-RE_RULES = recomp("^t\[""([^\]]+)""\] = ""([^\]]+)""$",RE_OPTIONS)
-    
-DEBUG = False
+class Translate():
+    def __init__(self):
+        self.database_setup()
+        self.parse_exercise_files()
+        self.update()
+        self.database_close()
 
-class ParseExerciseFiles():
-    def __init__(self,path):
-        self.texts = {}
-        self.traverse_folder()
-        self.path = path
-        if DEBUG:
-            print pprint.pprint(self.texts)
+    def database_setup(self):
+        self.conn = sqlite3.connect('translations.sqlite')
+        self.cursor = self.conn.cursor()
         
-    def traverse_folder(self):
-        """
-        Finds all html files and parses them
-        """
-        for paths,dirs,files in os.walk(self.path):
-            for exercise_file in files:
-                if exercise_file.endswith("html"):
-                    with open(os.path.join(self.path,exercise_file)) as f:
-                        self.parse_exercise_html(f.read(),exercise_file)
+        # tworzę tabelę jeżeli jeszcze nie istnieje
+        self.cursor.execute(
+        '''
+        create table if not exists translations (
+            content text unique, 
+            translation text,
+            filename text, 
+            has_translation boolean default 0,
+            valid_h text,
+            valid_u text,
+            used boolean default 0
+        )
+        ''')
+        self.sql = self.cursor.execute
+        self.conn.commit()
 
-    def parse_exercise_html(self,html,filename):
-        """
-        Searches html for different tags
-        """
-        for t in self.parse_text(html):
-            if self.is_valid_to_translate_heurestics_heurestics(t):
-                if t not in self.texts:
-                    self.texts[t] = {filename,}
-                else:
-                    self.texts[t].add(filename)
+    def parse_exercise_files(self):
+        reg = RegexMass(EXERCISES_PATH)
+        
+        for r in TRANSLATION_TAGS_RE:
+            texts = reg.findall(r)
+            for text in texts:
+                # sprawdzam czy tekst jest mniej więcej ludzki
+                content = text[0]
+                translation = text[1]
+                valid_h = self.is_valid_to_translate_heurestics(text[0])
+
+                # próbuje dodać wiersz do danych
+                try:
+                    self.sql("INSERT INTO translations (content, translation, filename, valid_h) VALUES (?,?,?,?)",
+                             (content, content, translation, valid_h))
+                # jeżeli się nie udało to na pewno kwestia powielonego klucza
+                # wtedy tylko updatuje listę plików
+                except sqlite3.IntegrityError:
+                    # sprawdzam jakie pliki już były zapisane
+                    filenames = self.sql("SELECT filename FROM translations WHERE content = ?",(content,)).fetchone()[0]
                     
-    def parse_text(self,html):
-        s = []
-        for regex in RE_INCLUDE:
-            s.extend(regex.findall(html))
-        return list(set(s))
-
-    def is_valid_to_translate_heurestics(self,html,debug=False):
+                    # rozdzielam i znowu sklejam alfabetycznie
+                    filenames = filenames.split(",")
+                    filenames.append(text[1])
+                    filenames = ",".join(sorted(list(set(filenames))))
+                    self.sql("UPDATE translations SET filename = ?, valid_h = ? WHERE content = ?",(filenames, valid_h, content))
+        self.conn.commit()
+    
+    def update(self):
+        def export_to_csv():
+            sqlite_export.export("translations.sqlite","translations","translation.csv")
+        
+        import sqlite_export
+        
+        if os.path.exists("translation.csv"):
+            # otwiera plik
+            # dodaje tłumaczenia, których nie ma w bazie
+            # update'uje wszystkie tłumaczenia z pliku
+            pass
+            
+        export_to_csv()
+    
+    def add_new(self):
+        pass
+        
+    def update_old(self):
+        pass
+    
+    def database_close(self):
+        self.conn.close()
+        
+    def is_valid_to_translate_heurestics(self,html):
         """
         Checks if the text is valid for translation
         """
         for rule in CLEAN_RULES:
-            if debug == True:
-                print html
             html = re.sub(rule,"",html)
         
         if len(html) > 0:
             return True
         else:
             return False
+
+t = Translate()
